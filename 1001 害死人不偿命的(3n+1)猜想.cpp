b0a10882fedb01ee415a03e19ddeb6dc8ef99f99
Z-demo <32590329+Z-demo@users.noreply.github.com>
2020-02-25 12:47:22 +0800

    Create 1001 害死人不偿命的(3n+1)猜想.cpp

---
+++ 1001 害死人不偿命的(3n+1)猜想.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    //cout << "请输入n的值!\n";
+    cin >> n;
+    int i = 0;
+    if (n <= 0)
+        exit(0);
+    while (n != 1) {
+        if (n % 2 == 0) {//为偶数        
+                n = n / 2;
+                i++;
+        }
+        else if(n % 2 == 1) {//为奇数
+                n = (3 * n + 1) / 2;
+                i++;
+        }
+    }
+    cout << i << endl;
+    //cout << "经过" << i << "轮" << "n=" << n << endl;
+}
