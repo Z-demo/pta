714c45c1137585f1d5f12949149b5387e11ae45f
Z-demo <32590329+Z-demo@users.noreply.github.com>
2020-02-29 23:00:32 +0800

    Update 1004成绩排名.cpp

--- 1004成绩排名.cpp
+++ 1004成绩排名.cpp
@@ -2,45 +2,54 @@
 #include <vector>
 #include <string>//可以将字符串转化为整型
 using namespace std;
+// PTA.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
 
-void solve_1004() {
+#include <iostream>
+#include <vector>
+#include <string>//可以将字符串转化为整型
+using namespace std;
+
+struct Student04 {
+    string name;
+    string pNum;
+    int grade;
+};
+void solve_1004_2() {
     int n;
-    int l = 3;
-    int maxIndex, minIndex;
     int max = 0, min = 101;
     //scanf("%d", &x);
     cin >> n;
-    vector<vector<string>> infos;
-    //getchar();
+    Student04 maxStu, minStu;
     for (int i = 0; i < n; ++i) {
         vector<string> info;
         string name, num;
         int grade;
         cin >> name >> num >> grade;
-        info.push_back(name);
-        info.push_back(num);
-        info.push_back(to_string(grade));
-        infos.push_back(info);
- 
+
         if (grade > max) {
             max = grade;
-            maxIndex = i;
+            maxStu.name = name;
+            maxStu.pNum = num;
+            maxStu.grade = grade;
         }
         if (grade < min) {
             min = grade;
-            minIndex = i;
+
+            minStu.name = name;
+            minStu.pNum = num;
+            minStu.grade = grade;
         }
     }
-    vector<string> maxInfo=infos.at(maxIndex);
-    vector<string> minInfo= infos.at(minIndex);
-    cout << maxInfo.at(0) << " " << maxInfo.at(1) << endl;
-    cout << minInfo.at(0) << " " << minInfo.at(1) << endl;
-
-  
+    cout << maxStu.name << " " << maxStu.pNum<<endl;
+    cout << minStu.name << " " << minStu.pNum << endl;
 }
 
 int main()
 {       
-    solve_1004();
+    solve_1004_2();
     return 0;
 }
+
+
+
