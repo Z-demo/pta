a624c8f7408d6117807300b723bc092e2ca0cdbd
Z-demo <32590329+Z-demo@users.noreply.github.com>
2020-02-29 21:49:33 +0800

    Update 1002 写出这个数.cpp
    读入一个正整数 n，计算其各位数字之和，用汉语拼音写出和的每一位数字。
    
    输入格式：
    每个测试输入包含 1 个测试用例，即给出自然数 n 的值。这里保证 n 小于 10
    ​100
    ​​ 。
    
    输出格式：
    在一行内输出 n 的各位数字之和的每一位，拼音数字间有 1 空格，但一行中最后一个拼音数字后没有空格。
    
    输入样例：
    1234567890987654321123456789
    
          
        
    输出样例：
    yi san wu

--- 1002 写出这个数.cpp
+++ 1002 写出这个数.cpp
@@ -3,10 +3,17 @@ using namespace std;
 
 int main()
 {
+    /*
+    1.求和
+    2.取这个和的各位
+    
+    note:字符数组末尾有一个'\0'
+    */
+    
     string d[] = { "ling","yi" };
     const char* p[] = { "ling","yi","er","san","si","wu","liu","qi","ba","jiu" };
     char a[101];
-    cin >> a;
+    cin >> a; //字符数组 或者string接收 一行数据
     int i = 0,sum=0;
     while (a[i] != '\0') {
         sum += a[i] - '0';
@@ -17,7 +24,7 @@ int main()
         return 0;
     }
    // cout << "sum=" << sum << endl;
-    int j = 0, b[11];
+    int j = 0, b[11];//可以用集合 或者动态数组
     while (sum != 0) {
         b[j++] = sum % 10;
         sum = sum / 10;
